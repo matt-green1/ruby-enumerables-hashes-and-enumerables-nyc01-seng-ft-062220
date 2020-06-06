a892a2c541001ebd45fa6f6da7b3dc3259fc7bc3
Matt Green <magreen118@gmail.com>
2020-06-06 04:39:56 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,6 +10,8 @@ def select_winner(passengers)
   winner = ""
   passengers.each do |key, value|
     if key == :suite_a && value.start_with?("A")
+      winner = value  
+    end
   end
   
   # add the code snippet here!
